97ef5e373165a993697d84449ba2bfbda2374244
benjamin-ll-conseil <77789470+benjamin-ll-conseil@users.noreply.github.com>
2022-09-13 18:48:56 +0200

    :sparkles: Cart Controller

---
+++ src/Controller/CartController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Products;
+use App\Repository\ProductsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/cart', name: 'cart_')]
+class CartController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(SessionInterface $session, ProductsRepository $productsRepository ): Response
+    {
+        $cart = $session->get("cart", []);
+
+        // On "fabrique" les données
+        $cartData = [];
+        $total = 0;
+
+        foreach($cart as $id => $quantity){
+            $product = $productsRepository->find($id);
+            $cartData[] = [
+                "product" => $product,
+                "quantity" => $quantity
+            ];
+            $total += $product->getPrice() * $quantity;
+        }
+
+        return $this->render('cart/index.html.twig', compact("cartData", "total"));
+    }
+
+    #[Route('/add/{id}', name: 'add')]
+    public function add(Products $product, SessionInterface $session)
+    {
+        // On récupère le panier actuel
+        $cart = $session->get("cart", []);
+        $id = $product->getId();
+
+        if(!empty($cart[$id])){
+            $cart[$id]++;
+        }else{
+            $cart[$id] = 1;
+        }
+
+        // On sauvegarde dans la session
+        $session->set("cart", $cart);
+
+        return $this->redirectToRoute("cart_index");
+        
+    }
+
+    #[Route('/remove/{id}', name: 'remove')]
+    public function remove(Products $product, SessionInterface $session)
+    {
+        // On récupère le panier actuel
+        $cart = $session->get("cart", []);
+        $id = $product->getId();
+
+        if(!empty($cart[$id])){
+            if($cart[$id] > 1){
+                $cart[$id]--;
+            }else{
+                unset($cart[$id]);
+            }
+        }
+
+        // On sauvegarde dans la session
+        $session->set("cart", $cart);
+
+        return $this->redirectToRoute("cart_index");
+    }
+        
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete(Products $product, SessionInterface $session)
+    {
+        // On récupère le panier actuel
+        $cart = $session->get("cart", []);
+        $id = $product->getId();
+
+        if(!empty($cart[$id])){
+            unset($cart[$id]);
+        }
+
+        // On sauvegarde dans la session
+        $session->set("cart", $cart);
+
+        return $this->redirectToRoute("cart_index");
+    }
+
+    #[Route('/delete', name: 'delete')]
+    public function deleteAll(SessionInterface $session)
+    {
+        $session->remove("cart");
+
+        return $this->redirectToRoute("cart_index");
+    }
+}
--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -6,6 +6,7 @@ use App\Entity\Products;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 #[Route('/products', name: 'products_')]
 class ProductsController extends AbstractController
@@ -17,8 +18,31 @@ class ProductsController extends AbstractController
     }
     
     #[Route('/{slug}', name: 'details')]
-    public function details(Products $product): Response
+    public function details(Products $product, SessionInterface $session): Response
     {
+        $cart = $session->get("cart", []);
+        
         return $this->render('products/details.html.twig', compact('product') );
+    }
+
+    #[Route('/{slug}/add', name: 'add')]
+    public function add(Products $product, SessionInterface $session)
+    {
+        // On récupère le panier actuel
+        $cart = $session->get("cart", []);
+        $id = $product->getId();
+        $slug = $product->getSlug();
+
+        if(!empty($cart[$id])){
+            $cart[$id]++;
+        }else{
+            $cart[$id] = 1;
+        }
+
+        // On sauvegarde dans la session
+        $session->set("cart", $cart);
+        $this->addFlash('success', 'Ce produit à bien été ajouté à votre panier !');
+        return $this->redirectToRoute("products_details", ['slug'=> $slug]);
+        
     }
 }
