3b536e27436a16ffca96d3a2523b54ff2f241405
benjamin-ll-conseil <77789470+benjamin-ll-conseil@users.noreply.github.com>
2022-09-24 11:18:48 +0200

    :sparkles: Add subcategory path

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Categories;
+use App\Repository\CategoriesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,16 +12,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CategoriesController extends AbstractController
 {
     #[Route('/{slug}', name: 'list')]
-    public function list(Categories $category): Response
+    public function list(Categories $category, CategoriesRepository $categoriesRepository): Response
     {
-        //On va chercher la liste des produits de la catégorie
-        $products = $category->getProducts();
-
-        return $this->render('categories/list.html.twig', compact('category', 'products'));
+        //On va chercher la liste des categories enfants de la catégorie
+        $subcategories = $categoriesRepository->findBy(['parent' => $category->getId()]);
+        return $this->render('categories/list.html.twig', compact('category', 'subcategories'));
         // Syntaxe alternative
         // return $this->render('categories/list.html.twig', [
         //     'category' => $category,
         //     'products' => $products
         // ]);
+    }
+    #[Route('/{parentSlug}/{slug}', name: 'sublist')]
+    public function sublist(Categories $category, CategoriesRepository $categoriesRepository): Response
+    {
+        //On va chercher la liste des produits de la catégorie
+        $products = $category->getProducts();
+        return $this->render('categories/sublist.html.twig', compact('category', 'products'));
+        
     }
 }
